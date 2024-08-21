e574cfa1904360a350aa1831689e414750ad3d41
suzhenjiang <suzhenjiang1031@gmail.com>
2024-08-21 18:28:26 +0800

    String Interpolation

---
+++ Inheritance and String/String Interpolation.py
@@ -0,0 +1,8 @@
+from math import pi
+'pi starts with ' + str(pi) + '...'
+print('pi starts with ' + str(pi) + '...')
+print(f'pi starts with {pi}...')
+
+from fractions import Fraction
+half = Fraction(1, 2)
+f'half of half is {half * half}'
