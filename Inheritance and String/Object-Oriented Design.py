22e1cb37e36334a851a4bfde1afdd6ec987c1700
suzhenjiang <suzhenjiang1031@gmail.com>
2024-08-21 18:27:28 +0800

    Object-Oriented Design

---
+++ Inheritance and String/Object-Oriented Design.py
@@ -0,0 +1,56 @@
+class Account:
+    """An account has a balance and a holder.
+    All accounts share a common interest rate."""
+
+    interest = 0.02    # A class attribute
+
+    def __init__(self, account_holder):
+        self.balance = 0
+        self.holder = account_holder
+
+    def deposit(self, amount):
+        """Add amount to balance."""
+        self.balance = self.balance + amount
+        return self.balance
+
+    def withdraw(self, amount):
+        """Subtract amount from balance."""
+        if amount > self.balance:
+            return 'Insufficient funds'
+        self.balance = self.balance - amount
+        return self.balance
+
+class CheckingAccount(Account):
+    interest = 0.01
+    withdraw_fee = 1
+
+    def withdraw(self, amount):
+        return Account.withdraw(self, amount + self.withdraw_fee)
+
+class Bank:
+    """A bank *has* accounts."""
+    def __init__(self):
+        self.accounts = []
+
+    def open_account(self, holder, amount, kind=Account):
+        account = kind(holder)
+        account.deposit(amount)
+        self.accounts.append(account)
+        return account
+
+    def pay_interest(self):
+        for a in self.accounts:
+            a.deposit(a.balance * a.interest)
+
+    def too_big_to_fail(self):
+        return len(self.accounts) > 1
+
+class SavingsAccount(Account):
+    deposit_fee = 2
+    def deposit(self, amount):
+        return Account.deposit(self, amount - self.deposit_fee)
+
+class AsSeenOnTVAccount(CheckingAccount, SavingsAccount):
+    def __init__(self, account_holder):
+        self.holder = account_holder
+        self.balance = 1   # A free dollar!
